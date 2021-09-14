c270b793aa29ae76318b9166f3812ff9ff006845
LFallert <lfallert13@yahoo.com>
2021-09-13 20:41:13 -0500

    fixed thymeleaf exercises

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Chris Bay
@@ -20,11 +22,22 @@ public class EventController {
 
     private static List<Event> events = new ArrayList<>();
 
+//    @GetMapping
+//    public String displayAllEvents(Model model) {
+//        model.addAttribute("title", "All Events");
+//        model.addAttribute("events", events);
+//        return "events/index";
+//    }
+
     @GetMapping
     public String displayAllEvents(Model model) {
-        model.addAttribute("title", "All Events");
-        model.addAttribute("events", events);
+        Map<String, String> eventsMap = new HashMap<>();
+        eventsMap.put("Strange Loop", "Cool STL Tech conference");
+        eventsMap.put("Code with Pride", "LC event for the LGBTQ+ community");
+        eventsMap.put("CoderGirl", "Cool thing where Mike plays weird music");
+        model.addAttribute("events", eventsMap);
         return "events/index";
+
     }
 
     @GetMapping("create")
